32a60a4187b789ac5465124147c01315acce2662
Abdullah Karagöz <abdullahkaragoz30@gmail.com>
2023-09-30 20:22:35 +0300

    -IdentityServer-Updates

---
+++ IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,42 @@
+﻿using FreeCourse.IdentityServer.Models;
+using IdentityModel;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var exitsUser = await _userManager.FindByNameAsync(context.UserName);
+            if (exitsUser == null)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string> { "email veya şifreniz yanlış!" });
+                context.Result.CustomResponse = errors;
+                return;
+            }
+
+            var passwordCheck = await _userManager.CheckPasswordAsync(exitsUser, context.Password);
+            if (passwordCheck == false)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string> { "Şifreniz yanlış!" });
+                context.Result.CustomResponse = errors;
+                return;
+            }
+
+            context.Result = new GrantValidationResult(exitsUser.Id.ToString(), OidcConstants.AuthenticationMethods.Password);
+        }
+    }
+}
--- Services/Catalog/Course.Services.Catalog/Startup.cs
+++ Services/Catalog/Course.Services.Catalog/Startup.cs
@@ -1,7 +1,9 @@
 using FreeCourse.Services.Catalog.Services;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,10 +24,13 @@ namespace FreeCourse.Services.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ICategoryService,CategoryService>();
-            services.AddScoped<ICourseService,CourseService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<ICourseService, CourseService>();
             services.AddAutoMapper(typeof(Startup));
-            services.AddControllers();
+            services.AddControllers(opt=>
+            {
+                opt.Filters.Add(new AuthorizeFilter());
+            });
 
             services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
 
@@ -37,6 +42,13 @@ namespace FreeCourse.Services.Catalog
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Course.Services.Catalog", Version = "v1" });
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_catalog";
+                options.RequireHttpsMetadata = false;
             });
         }
 
@@ -51,7 +63,7 @@ namespace FreeCourse.Services.Catalog
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
