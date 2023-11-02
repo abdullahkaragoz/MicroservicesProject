7c35c4cb07ee33912423e0e4fffa5a77bfdc4a63
Abdullah Karagöz <abdullah.karagoz@outlook.com>
2023-11-02 15:13:45 +0300

    -OrderService

---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,24 @@
+﻿using FreeCourse.Web.Models.Orders;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        /// <summary>
+        /// Senkron İletişim => Direk order microservice ine istek yapılacak
+        /// </summary>
+        /// <param name="checkoutInfoInput"></param>
+        /// <returns></returns>
+        Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput);
+        /// <summary>
+        /// Asenkron iletişim => Sipariş bilgileri rabbitMQ ya gönderilecek
+        /// </summary>
+        /// <param name="checkoutInfoInput"></param>
+        /// <returns></returns>
+        Task<OrderSuspendViewModel> SuspendOrder(CheckoutInfoInput checkoutInfoInput);
+
+        Task<List<OrderViewModel>> GetAllOrders();
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/OrderService.cs
@@ -0,0 +1,148 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Shared.Services;
+using FreeCourse.Web.Models.FakePayments;
+using FreeCourse.Web.Models.Orders;
+using FreeCourse.Web.Services.Interfaces;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services
+{
+    public class OrderService : IOrderService
+    {
+        #region Members
+        private readonly HttpClient _httpClient;
+        private readonly IPaymentService _paymentService;
+        private readonly IBasketService _basketService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public OrderService(HttpClient httpClient, IPaymentService paymentService, IBasketService basketService, ISharedIdentityService sharedIdentityService)
+        {
+            _httpClient = httpClient;
+            _paymentService = paymentService;
+            _basketService = basketService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        #endregion
+
+        #region Methods
+        public async Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput)
+        {
+            var basket = await _basketService.Get();
+
+            var paymentInfoInput = new PaymentInfoInput()
+            {
+                CardName = checkoutInfoInput.CardName,
+                CardNumber = checkoutInfoInput.CardNumber,
+                CVV = checkoutInfoInput.CVV,
+                Expiration = checkoutInfoInput.Expiration,
+                TotalPrice = basket.TotalPrice
+            };
+
+            var responsePayment = await _paymentService.ReceivePayment(paymentInfoInput);
+
+            if (!responsePayment)
+            {
+                return new OrderCreatedViewModel() { Error = "Ödeme alınamadı", IsSuccesful = false };
+            }
+
+            OrderCreateInput orderCreateInput = new()
+            {
+                BuyerId = _sharedIdentityService.GetUserId,
+                Address = new AddressCreateInput()
+                {
+                    Province = checkoutInfoInput.Province,
+                    District = checkoutInfoInput.District,
+                    Line = checkoutInfoInput.Line,
+                    Street = checkoutInfoInput.Street,
+                    ZipCode = checkoutInfoInput.ZipCode
+                }
+            };
+
+            basket.BasketItems.ForEach(b =>
+            {
+                OrderItemCreateInput orderItem = new()
+                {
+                    ProductId = b.CourseId,
+                    PictureUrl = "",
+                    Price = b.Price,
+                    ProductName = b.CourseName
+                };
+            });
+
+            var response = await _httpClient.PostAsJsonAsync<OrderCreateInput>("orders", orderCreateInput);
+            if (!response.IsSuccessStatusCode)
+            {
+                return new OrderCreatedViewModel() { Error = "Sipariş oluşturulamadı", IsSuccesful = false };
+            }
+
+            var orderCreatedViewModel = await response.Content.ReadFromJsonAsync<Response<OrderCreatedViewModel>>();
+
+            orderCreatedViewModel.Data.IsSuccesful = true;
+            await _basketService.Delete();
+            return orderCreatedViewModel.Data;
+        }
+
+        public async Task<List<OrderViewModel>> GetAllOrders()
+        {
+            var response = await _httpClient.GetFromJsonAsync<Response<List<OrderViewModel>>>("orders");
+
+            return response.Data;
+        }
+
+        public async Task<OrderSuspendViewModel> SuspendOrder(CheckoutInfoInput checkoutInfoInput)
+        {
+            var basket = await _basketService.Get();
+            OrderCreateInput orderCreateInput = new()
+            {
+                BuyerId = _sharedIdentityService.GetUserId,
+                Address = new AddressCreateInput
+                {
+                    Province = checkoutInfoInput.Province,
+                    District = checkoutInfoInput.District,
+                    Street = checkoutInfoInput.Street,
+                    Line = checkoutInfoInput.Line,
+                    ZipCode = checkoutInfoInput.ZipCode
+                },
+            };
+
+            basket.BasketItems.ForEach(x =>
+            {
+                var orderItem = new OrderItemCreateInput
+                {
+                    ProductId = x.CourseId,
+                    Price = x.GetCurrentPrice,
+                    PictureUrl = "",
+                    ProductName = x.CourseName
+                };
+                orderCreateInput.OrderItems.Add(orderItem);
+            });
+
+            var paymentInfoInput = new PaymentInfoInput()
+            {
+                CardName = checkoutInfoInput.CardName,
+                CardNumber = checkoutInfoInput.CardNumber,
+                Expiration = checkoutInfoInput.Expiration,
+                CVV = checkoutInfoInput.CVV,
+                TotalPrice = basket.TotalPrice,
+                Order = orderCreateInput
+            };
+
+            var responsePayment = await _paymentService.ReceivePayment(paymentInfoInput);
+
+            if (!responsePayment)
+            {
+                return new OrderSuspendViewModel() { Error = "Ödeme alınamadı", IsSuccessful = false };
+            }
+
+            await _basketService.Delete();
+            return new OrderSuspendViewModel() { IsSuccessful = true };
+
+        }
+        #endregion
+
+    }
+}
